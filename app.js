0257e5c7806ec0907e57a764cf3729a67249fa91
M1keCodingProjects <mikif2002@gmail.com>
2022-10-13 12:42:59 +0200

    Decided to abandon lateral chain construction in favor of image recognition, as per original idea.

--- app.js
+++ app.js
@@ -1,23 +1,10 @@
 // Hooray! (derogatory)
-const lateralChainTextContainer = document.getElementById('lateralChainStructure');
 const aaNamesSelectorsContainer = document.getElementById('nameSelectors-container');
 const namesSelect               = aaNamesSelectorsContainer.children[0];
 const acronymSelect             = aaNamesSelectorsContainer.children[1];
 const letterSelect              = aaNamesSelectorsContainer.children[2];
 
-const lateralChainStructure = [];
-
-const addGroup = (groupName) => {
-    lateralChainTextContainer.innerHTML += `${lateralChainStructure.length ? "-" : ""}${groupName}`;
-    lateralChainStructure.push(groupName);
-};
-
-const removeGroup = () => {
-    lateralChainStructure.pop();
-    lateralChainTextContainer.innerHTML = `-${lateralChainStructure.join("-")}`;
-};
-
-const shuffleSelectOptions = (selectObj) => {
+const shuffleSelectOptions = selectObj => {
     const options = Array.from(selectObj.options);
     options.forEach(option => {
         const randomOption = options[Math.floor(Math.random() * options.length)];
