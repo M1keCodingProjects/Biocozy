733dee096fb0c1380ae443df55a1175f4fd570be
M1keCodingProjects <mikif2002@gmail.com>
2022-10-13 12:22:26 +0200

    Added shuffle functionality for aa names.

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 // Hooray! (derogatory)
 const lateralChainTextContainer = document.getElementById('lateralChainStructure');
+const aaNamesSelectorsContainer = document.getElementById('nameSelectors-container');
+const namesSelect               = aaNamesSelectorsContainer.children[0];
+const acronymSelect             = aaNamesSelectorsContainer.children[1];
+const letterSelect              = aaNamesSelectorsContainer.children[2];
+
 const lateralChainStructure = [];
-const groupAddingButtons = document.getElementsByClassName('btn-addGroup'); //dunno if needed
 
 const addGroup = (groupName) => {
-    lateralChainTextContainer.innerHTML += `-${groupName}`;
+    lateralChainTextContainer.innerHTML += `${lateralChainStructure.length ? "-" : ""}${groupName}`;
     lateralChainStructure.push(groupName);
 };
 
 const removeGroup = () => {
     lateralChainStructure.pop();
-    lateralChainTextContainer.innerHTML = lateralChainStructure.length ? "-" : "";
-    lateralChainTextContainer.innerHTML += lateralChainStructure.join("-");
-}
+    lateralChainTextContainer.innerHTML = `-${lateralChainStructure.join("-")}`;
+};
+
+const shuffleSelectOptions = (selectObj) => {
+    const options = Array.from(selectObj.options);
+    options.forEach(option => {
+        const randomOption = options[Math.floor(Math.random() * options.length)];
+        selectObj.insertBefore(randomOption, option);
+    });
+};
+
+const shuffleNames = _=> {
+    shuffleSelectOptions(namesSelect);
+    shuffleSelectOptions(acronymSelect);
+    shuffleSelectOptions(letterSelect);
+};
