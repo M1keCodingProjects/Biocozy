4d31c7064506bc5ac0ccd664a9f6b53a7d438d96
M1keCodingProjects <mikif2002@gmail.com>
2022-10-15 00:58:50 +0200

    Wild restyle.

--- app.js
+++ app.js
@@ -1,19 +1,1 @@
 // Hooray! (derogatory)
-const aaNamesSelectorsContainer = document.getElementById('nameSelectors-container');
-const namesSelect               = aaNamesSelectorsContainer.children[0];
-const acronymSelect             = aaNamesSelectorsContainer.children[1];
-const letterSelect              = aaNamesSelectorsContainer.children[2];
-
-const shuffleSelectOptions = selectObj => {
-    const options = Array.from(selectObj.options);
-    options.forEach(option => {
-        const randomOption = options[Math.floor(Math.random() * options.length)];
-        selectObj.insertBefore(randomOption, option);
-    });
-};
-
-const shuffleNames = _=> {
-    shuffleSelectOptions(namesSelect);
-    shuffleSelectOptions(acronymSelect);
-    shuffleSelectOptions(letterSelect);
-};
