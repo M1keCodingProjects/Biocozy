ab6168ea43e280b288d125856263036e1f6e4561
M1keCodingProjects <mikif2002@gmail.com>
2022-12-13 22:01:20 +0100

    New page setup.

---
+++ SCRIPTS/urea.js
@@ -0,0 +1,9 @@
+import GameRoundManager from "./gameRound.js";
+
+const rollBehaviour = function() {
+    //...
+}
+
+const gameRound = new GameRoundManager("UREA", "", [
+    //...
+], rollBehaviour);
